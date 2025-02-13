8300fa67cd76c738b19dc591f3bf5e958ba63600
Jakub Wąsowicz-Kiryło <jakub.wasowicz-kirylo@troteclaser.com>
2025-02-13 16:45:55 +0100

    Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MenubarModule, PanelModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tulimy-map';
+  protected items: MenuItem[] = [
+    { label: 'Strona główna' },
+    { label: 'O projekcie' },
+    { label: 'O Tulimy' },
+    { label: 'Kontakt' },
+  ];
 }
