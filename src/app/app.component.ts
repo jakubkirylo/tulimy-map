ce926bef8fffaa7686e9e6e4da9d7f43f9f74bf9
Jakub Wąsowicz-Kiryło <jakub.wasowicz-kirylo@troteclaser.com>
2025-02-18 21:09:59 +0100

    Carousel - still some issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-import { CarouselModule } from 'primeng/carousel';
+import { Carousel, CarouselModule } from 'primeng/carousel';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
